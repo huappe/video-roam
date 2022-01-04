c7aaa1ac7895ba214e089495cf3104c1925351b6
huappe <155170805+huappe@users.noreply.github.com>
2022-01-04 15:42:49 +0000

    merge from 1.2

--- roam/src/GC_Energy.cpp
+++ roam/src/GC_Energy.cpp
@@ -108,4 +108,109 @@ construct8Graph(const cv::Mat& img, const cv::Mat& mask,
 
 
 // TODO: why raw pointer and undeclared in header?
-// ----------------------------------------------
+// ---------------------------------------------------------------------------
+static void estimateSegmentation(std::shared_ptr<GraphType> graph, cv::Mat &mask)
+// ---------------------------------------------------------------------------
+{
+    graph->maxflow();
+    
+    #pragma omp parallel for
+    for(auto y = 0; y < mask.rows; ++y)
+        for(auto x = 0; x < mask.cols; ++x)
+        {
+            const auto &val = mask.at<uchar>(y, x);
+            if(val == GC_Energy::GCClasses::GC_PR_BGD || val == GC_Energy::GCClasses::GC_PR_FGD)
+            {
+                if (graph->what_segment(y * mask.cols + x /*vertex index*/) == GraphType::SOURCE)
+                    mask.at<uchar>(y, x) = cv::GC_PR_FGD;
+                else
+                    mask.at<uchar>(y, x) = cv::GC_PR_BGD;
+            }
+        }
+}
+
+// TODO: why undeclared in header?
+// ---------------------------------------------------------------------------
+static double calcBeta(const cv::Mat &img)
+// ---------------------------------------------------------------------------
+{
+    double beta = 0;
+
+    #pragma omp parallel for reduction(+:beta)
+    for(auto y = 0; y < img.rows; ++y)
+        for(auto x = 0; x < img.cols; ++x)
+        {
+            const cv::Vec3d &color = img.at<cv::Vec3b>(y,x);
+            if(x > 0) // left
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y, x - 1));
+                beta += diff.dot(diff);
+            }
+            if(y > 0 && x > 0) // upleft
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y - 1, x - 1));
+                beta += diff.dot(diff);
+            }
+            if(y > 0) // up
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y - 1, x));
+                beta += diff.dot(diff);
+            }
+            if(y > 0 && x < img.cols - 1) // upright
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y - 1, x + 1));
+                beta += diff.dot(diff);
+            }
+        }
+
+    if(beta <= std::numeric_limits<double>::epsilon())
+        beta = 0;
+    else
+        beta = 1.f / (2 * beta/(4*img.cols*img.rows - 3*img.cols - 3*img.rows + 2) );
+
+    return beta;
+}
+
+// TODO: why undeclared in header?
+// ---------------------------------------------------------------------------
+static void
+calcNWeights(const cv::Mat& img, cv::Mat& leftW, cv::Mat& upleftW, cv::Mat& upW, cv::Mat& uprightW,
+             const double beta, const double gamma)
+// ---------------------------------------------------------------------------
+{
+    const double gammaDivSqrt2 = gamma / std::sqrt(2.0f);
+    leftW.create( img.rows, img.cols, CV_64FC1 );
+    upleftW.create( img.rows, img.cols, CV_64FC1 );
+    upW.create( img.rows, img.cols, CV_64FC1 );
+    uprightW.create( img.rows, img.cols, CV_64FC1 );
+
+    #pragma omp parallel for
+    for(auto y = 0; y < img.rows; ++y)
+        for(auto x = 0; x < img.cols; ++x)
+        {
+            const cv::Vec3d &color = img.at<cv::Vec3b>(y, x);
+            if(x - 1 >= 0) // left
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y, x - 1));
+                leftW.at<double>(y,x) = gamma * std::exp(-beta*diff.dot(diff));
+            }
+            else
+                leftW.at<double>(y,x) = 0;
+            if(x - 1 >= 0 && y - 1 >= 0) // upleft
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y - 1, x - 1));
+                upleftW.at<double>(y,x) = gammaDivSqrt2 * exp(-beta*diff.dot(diff));
+            }
+            else
+                upleftW.at<double>(y,x) = 0;
+            if(y-1 >= 0) // up
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y - 1, x));
+                upW.at<double>(y,x) = gamma * std::exp(-beta*diff.dot(diff));
+            }
+            else
+                upW.at<double>(y,x) = 0;
+            if(x+1 < img.cols && y-1 >= 0) // upright
+            {
+                const cv::Vec3d diff = color - static_cast<cv::Vec3d>(img.at<cv::Vec3b>(y - 1, x + 1));
+                uprightW.at<double>(y,x)
