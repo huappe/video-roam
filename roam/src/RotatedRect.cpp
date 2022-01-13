ab23f90fd70cba017b4f1cf39ba09abe75b7fb39
huappe <155170805+huappe@users.noreply.github.com>
2022-01-13 21:51:40 +0000

    Update press.html

--- roam/src/RotatedRect.cpp
+++ roam/src/RotatedRect.cpp
@@ -61,4 +61,111 @@ RotatedRect::RotatedRect()
     this->hack_flash = false;
     this->pA.x = this->pB.x = this->pC.x = this->pD.x = 0;
     this->pA.y = this->pB.y = this->pC.y = this->pD.y = 0;
-   
+    this->rect_up = true;
+    this->half_perimeter = 0.f;
+}
+
+// -----------------------------------------------------------------------------------
+RotatedRect::RotatedRect(const cv::Rect& cv_rect)
+// -----------------------------------------------------------------------------------
+{
+    this->hack_flash = false;
+    this->pD = cv::Point2f(cv_rect.tl()) + cv::Point2f(0, static_cast<float>(cv_rect.height));
+    this->pC = this->pD + cv::Point2f(static_cast<float>(cv_rect.width), 0);
+    const FLOAT_TYPE height = static_cast<FLOAT_TYPE>(cv_rect.height);
+
+    *this = RotatedRect(this->pD, this->pC, height, true);
+}
+
+
+// -----------------------------------------------------------------------------------
+RotatedRect::RotatedRect(const cv::Point2f &po_1, const cv::Point2f &po_2,
+                         const FLOAT_TYPE height, const bool rect_up)
+// -----------------------------------------------------------------------------------
+{
+
+    this->hack_flash = false;
+    this->rect_up = rect_up;
+
+    this->pD = po_1;
+    this->pC = po_2;
+
+    //std::cerr<<pD.x<<"=="<<pC.x<<" AND "<<pD.y<<"=="<<pC.y<<std::endl;
+
+    if(std::abs(pD.x-pC.x) < 0.0001)
+    {
+        pD.x += 0.0001f;
+        hack_flash = true;
+    }
+    if(std::abs(pD.y - pC.y) < 0.0001)
+    {
+        pD.y += 0.0001f;
+        hack_flash = true;
+    }
+
+    this->lCD = Line(this->pC, this->pD);
+    this->lBC = Line::perpLinePassPoint(this->lCD, this->pC);
+    this->lDA = Line::perpLinePassPoint(this->lCD, this->pD);
+
+    if (this->rect_up)
+        this->lAB = Line(this->lCD.M(), lCD.B()-height*std::sqrt(lCD.M()*lCD.M()+1));
+    else
+        this->lAB = Line(this->lCD.M(), lCD.B()+height*std::sqrt(lCD.M()*lCD.M()+1));
+
+    const FLOAT_TYPE xA = (lDA.B()-lAB.B()) / (lAB.M() - lDA.M());
+    const FLOAT_TYPE xB = (lBC.B()-lAB.B()) / (lAB.M() - lBC.M());
+    this->pA = cv::Point2f(xA,lAB.Y(xA));
+    this->pB = cv::Point2f(xB,lAB.Y(xB));
+
+    this->half_perimeter = static_cast<FLOAT_TYPE>( cv::norm(this->pA-this->pB) + cv::norm(this->pB-this->pC) );
+}
+
+// -----------------------------------------------------------------------------------
+cv::Vec3d RotatedRect::SumOver(const LineIntegralImage &verticalLineIntegralImage_) const
+// -----------------------------------------------------------------------------------
+{
+    const cv::Mat& verticalLineIntegralImage = verticalLineIntegralImage_.data;
+
+    std::vector<cv::Point2f> ups, dos;
+
+    if(this->pA.x <= this->pB.x && this->pA.x <= this->pC.x && this->pA.x <= this->pD.x)
+    {
+        // A is leftmost
+        for(int x = static_cast<int>(pA.x); x < static_cast<int>(pD.x); ++x)
+        {
+            ups.push_back(cv::Point2f(static_cast<float>(x), lAB.Y(static_cast<float>(x))));
+            dos.push_back(cv::Point2f(static_cast<float>(x), lDA.Y(static_cast<float>(x))));
+        }
+
+        for (int x = static_cast<int>(pD.x); x < static_cast<int>(pB.x); ++x)
+        {
+            ups.push_back(cv::Point2f(static_cast<float>(x), lAB.Y(static_cast<float>(x))));
+            dos.push_back(cv::Point2f(static_cast<float>(x), lCD.Y(static_cast<float>(x))));
+        }
+
+        for (int x = static_cast<int>(pB.x); x < static_cast<int>(pC.x); ++x)
+        {
+            ups.push_back(cv::Point2f(static_cast<float>(x), lBC.Y(static_cast<float>(x))));
+            dos.push_back(cv::Point2f(static_cast<float>(x), lCD.Y(static_cast<float>(x))));
+        }
+    }
+
+    if(this->pB.x<this->pA.x && this->pB.x<=this->pC.x && this->pB.x<=this->pD.x  )
+    {
+        // B is leftmost
+        for(int x = static_cast<int>(pB.x); x<static_cast<int>(pC.x); ++x)
+        {
+            ups.push_back(cv::Point2f(static_cast<float>(x), lBC.Y(static_cast<float>(x))));
+            dos.push_back(cv::Point2f(static_cast<float>(x), lAB.Y(static_cast<float>(x))));
+        }
+
+        for(int x = static_cast<int>(pC.x); x<static_cast<int>(pA.x); ++x)
+        {
+            ups.push_back(cv::Point2f(static_cast<float>(x), lCD.Y(static_cast<float>(x))));
+            dos.push_back(cv::Point2f(static_cast<float>(x), lAB.Y(static_cast<float>(x))));
+        }
+
+        for(int x = static_cast<int>(pA.x); x<static_cast<int>(pD.x); ++x)
+        {
+            ups.push_back(cv::Point2f(static_cast<float>(x), lCD.Y(static_cast<float>(x))));
+            dos.push_bac
