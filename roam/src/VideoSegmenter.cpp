6b593a2c842e4c904c5126347d53b146f2ed19b4
huappe <155170805+huappe@users.noreply.github.com>
2022-09-20 19:11:32 +0000

    Docstring improvements

--- roam/src/VideoSegmenter.cpp
+++ roam/src/VideoSegmenter.cpp
@@ -125,3 +125,83 @@ size_t addNodeToList(std::list<T>& added_list, const T &element, const int prev_
     }
     else
     {
+        added_list.insert(std::next(added_list.begin(), prev_node + 1), element);
+        return prev_node + 1;
+    }
+}
+
+// -----------------------------------------------------------------------------------
+void VideoSegmenter::reinitializeNode(Node &node, const Node &next_node, int prev_node, int node_idx, ContourElementsHelper &ce) const
+// -----------------------------------------------------------------------------------
+{
+    if(this->contour_init)
+    {
+        node = Node(node.GetCoordinates(), Node::Params(params.label_space_side));
+
+        if (params.use_gradients_unary)
+            node.AddUnaryTerm(ce.contour_gradient_unary);
+
+        if (params.use_norm_pairwise)
+            node.AddPairwiseTerm(ce.contour_norm_pairwise);
+
+        if(params.use_temp_norm_pairwise)
+        {
+            const cv::Ptr<ROAM::TempAnglePairwise> tempAnglePairwise = ROAM::TempAnglePairwise::createPairwiseTerm(ROAM::TempAnglePairwise::Params(this->params.temp_angle_weight));
+            const cv::Ptr<ROAM::TempNormPairwise> tempNormPairwise = ROAM::TempNormPairwise::createPairwiseTerm(ROAM::TempNormPairwise::Params(this->params.temp_norm_weight));
+
+            tempAnglePairwise->Init(cv::Mat(cv::Mat_<FLOAT_TYPE>(1,1) << -5.f * CV_PI), cv::Mat());
+            tempNormPairwise->Init(cv::Mat(cv::Mat_<FLOAT_TYPE>(1,1) << -1.f), cv::Mat());
+
+            addNodeToList(ce.tempangle_pairwises_per_contour_node, tempAnglePairwise, prev_node);
+            addNodeToList(ce.tempnorm_pairwises_per_contour_node, tempNormPairwise, prev_node);
+
+            node.AddPairwiseTerm(tempAnglePairwise);
+            node.AddPairwiseTerm(tempNormPairwise);
+
+            const cv::Point a = node.GetCoordinates();
+            const cv::Point b = next_node.GetCoordinates();
+
+            addNodeToList(ce.prev_angles, FLOAT_TYPE(std::atan2(b.y - a.y, b.x - a.x)), prev_node);
+            addNodeToList(ce.prev_norms, FLOAT_TYPE(cv::norm(b - a)), prev_node);
+        }
+
+        if(params.use_landmarks)
+        {
+            const cv::Ptr<ROAM::DistanceUnary> distanceUnary = ROAM::DistanceUnary::createUnaryTerm(
+                                ROAM::DistanceUnary::Params(this->params.landmark_to_node_weight));
+            distanceUnary->Init(cv::Mat());
+            distanceUnary->SetPastDistance(-1.f /*when a negative number is passed, the cost becomes 0: only for intiialization*/);
+
+            addNodeToList(ce.distance_unaries_per_contour_node, distanceUnary, prev_node);
+            node.AddUnaryTerm(distanceUnary);
+        }
+
+        if(params.use_green_theorem_term)
+        {
+            const cv::Ptr<ROAM::GreenTheoremPairwise> gtPairwise =
+                    ROAM::GreenTheoremPairwise::createPairwiseTerm(
+                    ROAM::GreenTheoremPairwise::Params(this->params.green_theorem_weight,
+                                                           this->contour->IsCounterClockWise(),
+                                                           prev_node, node_idx));
+
+            gtPairwise->Init(this->next_image, this->integral_negative_ratio_foreground_background_likelihood);
+
+            addNodeToList(ce.green_theorem_pairwises, gtPairwise, prev_node);
+            node.AddPairwiseTerm(gtPairwise);
+        }
+
+        if (params.use_snapcut_pairwise)
+        {
+            ROAM::SnapcutPairwise::Params snapcut_params(params.snapcut_weight,
+                                                         params.snapcut_sigma_color, params.snapcut_region_height,
+                                                         params.label_space_side, params.snapcut_number_clusters, true);
+
+            const cv::Ptr<ROAM::SnapcutPairwise> snapcutPairwise = ROAM::SnapcutPairwise::createPairwiseTerm(snapcut_params);
+            snapcutPairwise->Init(this->next_image, this->frame_mask/*intermediate_mask*/);
+
+            addNodeToList(ce.snapcut_pairwises_per_contour_node, snapcutPairwise, prev_node);
+
+            // I guesss I don't need to check this since frame_mask was already updated:
+            // if (!params.use_landmarks || !this->landmarks_tree->DPTableIsBuilt())
+            snapcutPairwise->InitializeEdge(node.GetCoordinates(),
+                                            next_node.GetCo
